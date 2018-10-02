24229e1439b4e19f707a9f88aa9cad20ffa7c797
beloved-processor-5901 <naqashmalik139@gmail.com>
2018-10-02 19:37:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  animal = 'cat'
   return animal
 }
 
@@ -16,7 +15,7 @@ function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  const two = 71
 }
 
 var funkyFunction = function() {
